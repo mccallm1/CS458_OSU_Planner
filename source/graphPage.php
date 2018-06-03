aa6d4d0c8b1983edef0bc8bc150e2acf1c8f6351
Miles McCall <mccallm@oregonstate.edu>
2018-06-03 16:09:40 -0700

    added graph page

--- source/graphPage.php
+++ source/graphPage.php
@@ -1,263 +1,203 @@
-<!doctype html>
-<html lang="en">
-
+<!DOCTYPE HTML>
+<html>
   <head>
     <meta charset="utf-8">
-    <title>OSU Planner</title>
-    <meta name="description" content="The HTML5 Herald">
+    <title>OSU Credit Plotter</title>
+    <meta name="description">
     <link rel="stylesheet" href="bootstrap-3.3.7-dist/css/bootstrap.css"/>
     <script src="https://code.jquery.com/jquery-3.1.0.min.js"></script>
     <script src="bootstrap-3.3.7-dist/js/bootstrap.min.js"></script>
-  </head>
 
-  <?php
-    // Connection
-    $dbhost = 'oniddb.cws.oregonstate.edu';
-    $dbname = 'mccallm-db';
-    $dbuser = 'mccallm-db';
-    $dbpass = 'JTOkIJcZBATl1x1X';
+    <?php
+      // Connection
+      $dbhost = 'oniddb.cws.oregonstate.edu';
+      $dbname = 'mccallm-db';
+      $dbuser = 'mccallm-db';
+      $dbpass = 'JTOkIJcZBATl1x1X';
 
-    $conn = new mysqli($dbhost, $dbuser, $dbpass, $dbname);
-      if ($conn->connect_errno) {
-        echo "Failed to connect to MySQL <br/>";
+      $conn = new mysqli($dbhost, $dbuser, $dbpass, $dbname);
+        if ($conn->connect_errno) {
+          echo "Failed to connect to MySQL <br/>";
+          exit;
+        }
+
+      // Variables
+        $fall_credits   = 0;
+        $winter_credits = 0;
+        $spring_credits = 0;
+
+        $fall_comp_sum   = 0;
+        $winter_comp_sum = 0;
+        $spring_comp_sum = 0;
+    ?>
+
+    <?php
+      // Student
+      $sql = "SELECT firstname,lastname,standing FROM Student WHERE id = '1' ";
+
+      if (!$result = $conn->query($sql)) {
+        echo "Error: Query failed to execute: <br/>";
+        exit;
+      }
+      if ($result->num_rows === 0) {
+        echo "Query empty. <br/>";
         exit;
       }
 
-    // Variables
-      $fall_credits   = 0;
-      $winter_credits = 0;
-      $spring_credits = 0;
+      $student = $result->fetch_assoc();
+    ?>
 
-      $fall_comp_sum   = 0;
-      $winter_comp_sum = 0;
-      $spring_comp_sum = 0;
-  ?>
 
-  <?php
-    // Student
-    $sql = "SELECT firstname,lastname,standing FROM Student WHERE id = '1' ";
 
-    if (!$result = $conn->query($sql)) {
-      echo "Error: Query failed to execute: <br/>";
-      exit;
-    }
-    if ($result->num_rows === 0) {
-      echo "Query empty. <br/>";
-      exit;
-    }
+    <?php
+      // Fall
+      $sql = "SELECT name,credits,course_time,course_completed FROM Course,Schedule WHERE Schedule.course_id = Course.id AND term = 'F14' LIMIT 4";
 
-    $student = $result->fetch_assoc();
-  ?>
-
-  <?php
-    // Fall
-    $sql = "SELECT name,credits,course_time,course_completed FROM Course,Schedule WHERE Schedule.course_id = Course.id AND term = 'F14' LIMIT 4";
-
-    if (!$result = $conn->query($sql)) {
-      echo "Error: Query failed to execute: <br/>";
-      exit;
-    }
-    if ($result->num_rows === 0) {
-      echo "Query empty. <br/>";
-      exit;
-    }
-
-    $i = 0;
-    while ($course = $result->fetch_assoc()) {
-      $fall_term[$i] = $course;
-      if ($course['course_completed'] > 0) {
-        $fall_credits += $course['credits'];
+      if (!$result = $conn->query($sql)) {
+        echo "Error: Query failed to execute: <br/>";
+        exit;
       }
-      $fall_comp_sum += $course['course_completed'];
-      $i++;
-    }
-  ?>
-  <?php
-    // Winter
-    $sql = "SELECT name,credits,course_time,course_completed FROM Course,Schedule WHERE Schedule.course_id = Course.id AND term = 'W15' LIMIT 3";
-
-    if (!$result = $conn->query($sql)) {
-      echo "Error: Query failed to execute: <br/>";
-      exit;
-    }
-    if ($result->num_rows === 0) {
-      echo "Query empty. <br/>";
-      exit;
-    }
-
-    $i = 0;
-    while ($course = $result->fetch_assoc()) {
-      $winter_term[$i] = $course;
-      if ($course['course_completed'] > 0) {
-        $winter_credits += $course['credits'];
+      if ($result->num_rows === 0) {
+        echo "Query empty. <br/>";
+        exit;
       }
-      $winter_comp_sum += $course['course_completed'];
-      $i++;
-    }
-  ?>
-  <?php
-    // Spring
-    $sql = "SELECT name,credits,course_time,course_completed FROM Course,Schedule WHERE Schedule.course_id = Course.id AND  term = 'SP15' LIMIT 4";
 
-    if (!$result = $conn->query($sql)) {
-      echo "Error: Query failed to execute: <br/>";
-      exit;
-    }
-    if ($result->num_rows === 0) {
-      echo "Query empty. <br/>";
-      exit;
-    }
-
-    $i = 0;
-    while ($course = $result->fetch_assoc()) {
-      $spring_term[$i] = $course;
-      if ($course['course_completed'] > 0) {
-        $spring_credits += $course['credits'];
+      $i = 0;
+      while ($course = $result->fetch_assoc()) {
+        $fall_term[$i] = $course;
+        if ($course['course_completed'] > 0) {
+          $fall_credits += $course['credits'];
+        }
+        $fall_comp_sum += $course['course_completed'];
+        $i++;
       }
-      $spring_comp_sum += $course['course_completed'];
-      $i++;
+    ?>
+    <?php
+      // Winter
+      $sql = "SELECT name,credits,course_time,course_completed FROM Course,Schedule WHERE Schedule.course_id = Course.id AND term = 'W15' LIMIT 3";
+
+      if (!$result = $conn->query($sql)) {
+        echo "Error: Query failed to execute: <br/>";
+        exit;
+      }
+      if ($result->num_rows === 0) {
+        echo "Query empty. <br/>";
+        exit;
+      }
+
+      $i = 0;
+      while ($course = $result->fetch_assoc()) {
+        $winter_term[$i] = $course;
+        if ($course['course_completed'] > 0) {
+          $winter_credits += $course['credits'];
+        }
+        $winter_comp_sum += $course['course_completed'];
+        $i++;
+      }
+    ?>
+    <?php
+      // Spring
+      $sql = "SELECT name,credits,course_time,course_completed FROM Course,Schedule WHERE Schedule.course_id = Course.id AND  term = 'SP15' LIMIT 4";
+
+      if (!$result = $conn->query($sql)) {
+        echo "Error: Query failed to execute: <br/>";
+        exit;
+      }
+      if ($result->num_rows === 0) {
+        echo "Query empty. <br/>";
+        exit;
+      }
+
+      $i = 0;
+      while ($course = $result->fetch_assoc()) {
+        $spring_term[$i] = $course;
+        if ($course['course_completed'] > 0) {
+          $spring_credits += $course['credits'];
+        }
+        $spring_comp_sum += $course['course_completed'];
+        $i++;
+      }
+    ?>
+
+    <script>
+    window.onload = function () {
+      var chart = new CanvasJS.Chart("chartContainer", {
+      	animationEnabled: true,
+      	title:{
+      		text: "<?php echo $student['firstname'] . ' ' . $student['lastname']; ?> - Career Progress "
+      	},
+      	axisY: {
+      		title: "Number of Credits",
+      		titleFontColor: "#4F81BC",
+      		lineColor: "#4F81BC",
+      		labelFontColor: "#4F81BC",
+      		tickColor: "#4F81BC"
+      	},
+      	toolTip: {
+      		shared: true
+      	},
+      	legend: {
+      		cursor:"pointer",
+      		itemclick: toggleDataSeries
+      	},
+
+      	data: [{
+      		type: "column",
+      		name: "Credits Earned",
+      		legendText: "Credits Earned",
+      		showInLegend: true,
+      		dataPoints:[
+      			{ label: "Fresh Fall", y: <?php echo $fall_credits; ?> },
+      			{ label: "Fresh Winter", y: <?php echo $winter_credits; ?> },
+      			{ label: "Fresh Spring", y: <?php echo $spring_credits; ?> },
+      			{ label: "Soph Fall", y: 148.77 },
+      			{ label: "Soph Winter", y: 101.50 },
+      			{ label: "Soph Spring", y: 97.8 },
+            { label: "Junior Fall", y: 148.77 },
+      			{ label: "Junior Winter", y: 101.50 },
+      			{ label: "Junior Spring", y: 97.8 },
+            { label: "Senior Fall", y: 148.77 },
+      			{ label: "Senior Winter", y: 101.50 },
+      			{ label: "Senior Spring", y: 97.8 }
+      		]
+      	},
+      	{
+      		type: "column",
+      		name: "Credits Taken",
+      		legendText: "Credits Taken",
+      		showInLegend: true,
+      		dataPoints:[
+            { label: "Fresh Fall", y: <?php echo $fall_comp_sum; ?> },
+      			{ label: "Fresh Winter", y: <?php echo $winter_comp_sum; ?> },
+      			{ label: "Fresh Spring", y: <?php echo $spring_comp_sum; ?> },
+      			{ label: "Soph Fall", y: 148.77 },
+      			{ label: "Soph Winter", y: 101.50 },
+      			{ label: "Soph Spring", y: 97.8 },
+            { label: "Junior Fall", y: 148.77 },
+      			{ label: "Junior Winter", y: 101.50 },
+      			{ label: "Junior Spring", y: 97.8 },
+            { label: "Senior Fall", y: 148.77 },
+      			{ label: "Senior Winter", y: 101.50 },
+      			{ label: "Senior Spring", y: 97.8 }
+      		]
+      	}]
+      });
+      chart.render();
+      function toggleDataSeries(e) {
+      	if (typeof(e.dataSeries.visible) === "undefined" || e.dataSeries.visible) {
+      		e.dataSeries.visible = false;
+      	}
+      	else {
+      		e.dataSeries.visible = true;
+      	}
+      	chart.render();
+      }
     }
-  ?>
+    </script>
+  </head>
 
   <body>
-    <div class="row">
-      <div class="col-sm-4">
-      </div>
-      <div class="col-sm-4">
-        <div class="well well-lg text-center" style="background-color:#d9edf7; overflow: auto;"><h1>
-          <?php echo $student['firstname'] . ' ' .
-                $student['lastname'] . "<br/>" .
-                $student['standing'];
-          ?>
-        </h1></div>
-      </div>
-      <div class="col-sm-4">
-      </div>
-    </div>
-
-    <div class="row">
-      <div class="col-sm-4">
-        <div class="panel panel-success">
-          <?php
-            if ($fall_comp_sum == sizeof($fall_term)) {
-              echo "<div class=\"panel-heading text-center\" style=\"background-color:#d6e9c6; overflow: auto;\">"; // Green
-            } else {
-              echo "<div class=\"panel-heading text-center\" style=\"background-color:#fcf8e3; overflow: auto;\">"; // Yellow
-            }
-            echo "<font color=\"black\"> <h2>Fall 2014</h2> </font></div>";
-          ?> <p><br></p>
-
-          <?php
-            foreach ($fall_term as $course) {
-              if ($course['course_completed'] == 0) {
-                echo "<div class=\"well well-xs text-center col-sm-offset-3\" style=\"background-color:#f2dede; overflow: auto;\">";
-              }
-              elseif ($course['course_completed'] == 2) {
-                echo "<div class=\"well well-xs text-center col-sm-offset-3\" style=\"background-color:#fcf8e3; overflow: auto;\">";
-              }
-              else {
-                echo "<div class=\"well well-xs text-center col-sm-offset-3\" style=\"background-color:#d6e9c6; overflow: auto;\">";
-              }
-
-                echo $course['name'] . ' - ' .
-                  $course['credits'] . " credits <br/>";
-
-              echo "</div>";
-            }
-          ?>
-
-          <div class="well well-xs text-left col-sm-offset-3" style="background-color:#d9edf7; overflow: auto;">
-            <?php
-              echo $fall_credits . " Term Credits" . "<br/>";
-              echo $fall_credits . " Credits Completed"  . "<br/>";
-            ?>
-          </div>
-        </div>
-      </div>
-
-    <div class="row">
-      <div class="col-sm-4">
-        <div class="panel panel-success" border-color="black">
-          <?php
-          if ($winter_comp_sum == sizeof($winter_term)) {
-            echo "<div class=\"panel-heading text-center\" style=\"background-color:#d6e9c6; overflow: auto;\">"; // Green
-          } else {
-            echo "<div class=\"panel-heading text-center\" style=\"background-color:#fcf8e3; overflow: auto;\">"; // Yellow
-          }
-            echo "<font color=\"black\"> <h2>Winter 2015</h2> </font> </div>";
-          ?> <p><br></p>
-
-          <?php
-            foreach ($winter_term as $course) {
-              if ($course['course_completed'] == 0) {
-                echo "<div class=\"well well-xs text-center col-sm-offset-3\" style=\"background-color:#f2dede; overflow: auto;\">";
-              }
-              elseif ($course['course_completed'] == 2) {
-                echo "<div class=\"well well-xs text-center col-sm-offset-3\" style=\"background-color:#fcf8e3; overflow: auto;\">";
-              }
-              else {
-                echo "<div class=\"well well-xs text-center col-sm-offset-3\" style=\"background-color:#d6e9c6; overflow: auto;\">";
-              }
-
-                echo $course['name'] . ' - ' .
-                  $course['credits'] . " credits <br/>";
-
-              echo "</div>";
-            }
-          ?>
-
-          <div class="well well-xs text-left col-sm-offset-3" style="background-color:#d9edf7; overflow: auto;">
-            <?php
-              echo $winter_credits . " Term Credits" . "<br/>";
-              echo $fall_credits + $winter_credits . " Credits Completed"  . "<br/>";
-            ?>
-          </div>
-        </div>
-      </div>
-
-    <div class="row">
-      <div class="col-sm-4">
-        <div class="panel panel-success">
-          <?php
-          if ($spring_comp_sum == sizeof($spring_term)) {
-            echo "<div class=\"panel-heading text-center\" style=\"background-color:#d6e9c6; overflow: auto;\">"; // Green
-          } else {
-            echo "<div class=\"panel-heading text-center\" style=\"background-color:#fcf8e3; overflow: auto;\">"; // Yellow
-          }
-            echo "<font color=\"black\"> <h2>Spring 2015</h2> </font> </div>";
-          ?> <p><br></p>
-
-          <?php
-            foreach ($spring_term as $course) {
-              if ($course['course_completed'] == 0) {
-                echo "<div class=\"well well-xs text-center col-sm-offset-3\" style=\"background-color:#f2dede; overflow: auto;\">";
-              }
-              elseif ($course['course_completed'] == 2) {
-                echo "<div class=\"well well-xs text-center col-sm-offset-3\" style=\"background-color:#fcf8e3; overflow: auto;\">";
-              }
-              else {
-                echo "<div class=\"well well-xs text-center col-sm-offset-3\" style=\"background-color:#d6e9c6; overflow: auto;\">";
-              }
-
-                echo $course['name'] . ' - ' .
-                  $course['credits'] . " credits <br/>";
-
-              echo "</div>";
-            }
-          ?>
-
-
-          <div class="well well-xs text-left col-sm-offset-3"  style="background-color:#d9edf7; overflow: auto;">
-            <?php
-              echo $spring_credits . " Term Credits" . "<br/>";
-              echo $fall_credits + $winter_credits + $spring_credits . " Credits Completed"  . "<br/>";
-            ?>
-          </div>
-        </div>
-      </div>
-
-
-
+    <div id="chartContainer" style="height: 300px; width: 100%;"></div>
+    <script src="https://canvasjs.com/assets/script/canvasjs.min.js"></script>
   </body>
-
 </html>
